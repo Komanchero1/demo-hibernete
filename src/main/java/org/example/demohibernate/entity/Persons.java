5594f433479d1103612c2f49ea60a70c5af6122b
Dmitriy <160003751+Komanchero1@users.noreply.github.com>
2024-12-16 21:32:27 +0400

    Update Persons.java

--- src/main/java/org/example/demohibernate/entity/Persons.java
+++ src/main/java/org/example/demohibernate/entity/Persons.java
@@ -4,31 +4,36 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-
+@Entity
 @Getter
 @Setter
-@Entity
 @IdClass(PersonId.class)
 public class Persons {
 
     @Id
     private String name;
+
     @Id
     private String surname;
+
     @Id
     private int age;
-    private String phone_number;
-    private String city_of_living;
+
+    private String phoneNumber; // Используйте camelCase
+
+    private String cityOfLiving; // Используйте camelCase
 
     public Persons() {
-
     }
 
-    public Persons(String name, String surname, int age, String phone_number, String city_of_living) {
+    public Persons(String name, String surname, int age, String phoneNumber, String cityOfLiving) {
         this.name = name;
         this.surname = surname;
         this.age = age;
-        this.phone_number = phone_number;
-        this.city_of_living = city_of_living;
+        this.phoneNumber = phoneNumber; // Исправлено
+        this.cityOfLiving = cityOfLiving; // Исправлено
     }
+
 }
+
+
