547a826b76fd255220eb4e96a36ab6a1ef68a867
Dmitriy <160003751+Komanchero1@users.noreply.github.com>
2024-12-16 21:30:31 +0400

    Update DataInitializer.java

--- src/main/java/org/example/demohibernate/config/DataInitializer.java
+++ src/main/java/org/example/demohibernate/config/DataInitializer.java
@@ -3,11 +3,12 @@ package org.example.demohibernate.config;
 import org.example.demohibernate.entity.Persons;
 import org.example.demohibernate.repository.PersonsRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-
-public class DataInitializer implements CommandLineRunner{
+@Component
+public class DataInitializer implements CommandLineRunner {
 
     private final PersonsRepository personsRepository;
 
@@ -16,10 +17,17 @@ public class DataInitializer implements CommandLineRunner{
         this.personsRepository = personsRepository;
     }
 
-   @Override
-    public void run(String... args) throws IOException {
-        personsRepository.save(new Persons("Olga", "Karpova", 30, "1234567890", "Moscow"));
-        personsRepository.save(new Persons("Dmitry", "Ivanov", 25, "0987654321", "Los Angeles"));
-        personsRepository.save(new Persons("Ivan", "Sergeev", 21, "0987654429", "Moscow"));
+    private void saveIfNotExists(Persons person) {
+        if (personsRepository.getPersonsByCity(person.getCityOfLiving()).isEmpty()) {
+            personsRepository.save(person);
+        }
     }
+
+    @Override
+    public void run(String... args) throws IOException {
+        saveIfNotExists(new Persons("Olga", "Karpova", 30, "1234567890", "Moscow"));
+        saveIfNotExists(new Persons("Dmitry", "Ivanov", 25, "0987654321", "Los Angeles"));
+        saveIfNotExists(new Persons("Ivan", "Sergeev", 21, "0987654429", "Moscow"));
+    }
+
 }
